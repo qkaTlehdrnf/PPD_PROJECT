dbffa5f1a4f493cf46ed533ba3f996ef97156c4b
Kim,Won-Joong <wonjoong11@naver.com>
2022-06-07 15:42:39 +0900

    0607

--- machine_layout.py
+++ machine_layout.py
@@ -32,23 +32,35 @@ class MachineTemplate:
 
 class BackgroundTemplate:
     def __init__(self, bg, inandout=None):
-        self.null_bg = bg.copy()#this is just blanck background
-        self.bg = bg#background of the factory, it is a 2D array
-        self.machine_dict = defaultdict(MachineTemplate)#machine code to machine template
-        self.product_machines = {}#machine line used for each product
-        self.machine_inandout = {}#machine code to in and out position
-        self.sole_bg = defaultdict(lambda: np.zeros(self.bg.shape))#machine code to machine template
+        self.null_bg = bg.copy()  # this is just blanck background
+        self.bg = bg  # background of the factory, it is a 2D array
+        self.machine_dict = defaultdict(
+            MachineTemplate
+        )  # machine code to machine template
+        self.product_machines = {}  # machine line used for each product
+        self.machine_inandout = {}  # machine code to in and out position
+        self.sole_bg = defaultdict(
+            lambda: np.zeros(self.bg.shape)
+        )  # machine code to machine template
+        self.collision = False
         if inandout == None:
             inandout = (
                 [np.shape(bg)[0] // 2, 0],
                 [np.shape(bg)[0] // 2, np.shape(bg)[1]],
             )
         self.In, self.out = inandout
+
+    def reset_bg(self):
+        self.bg = self.null_bg.copy()
+        for machine_code in self.machine_dict:
+            self.sole_bg[machine_code] = self.null_bg.copy()
+        self.collision = False
+
     def collide_check(self):
         if 2 in self.bg:
             self.collision = True
 
-    def product_line(self, product_name, machine_line):
+    def product_line(self, product_name, machine_line:list):
         for m in machine_line:
             assert m in self.machine_dict.keys()
         self.product_machines[product_name] = machine_line
@@ -70,25 +82,22 @@ class BackgroundTemplate:
     def machine_batch(self, position, machine_code):
         try:
             machine = self.machine_dict[machine_code]
+            self.sole_bg[machine_code] = self.null_bg.copy()
             self.sole_bg[machine_code][
-                position[0] : position[0]
-                + machine.occupy.shape[0],
-                position[1] : position[1]
-                + machine.occupy.shape[1],
+                position[0] : position[0] + machine.occupy.shape[0],
+                position[1] : position[1] + machine.occupy.shape[1],
             ] = machine.occupy
             self.bg = np.add(self.bg, self.sole_bg[machine_code])
             self.machine_inandout[machine_code] = [
-                [sum(x) for x in zip(machine.In, position)],[sum(x) for x in zip(machine.out, position)]
-                ]
+                [sum(x) for x in zip(machine.In, position)],
+                [sum(x) for x in zip(machine.out, position)],
+            ]
         except ValueError as e:
-            print(e)
-            raise BoundaryError(
-                "Machine exceeds boundary, machine position is {} and machine size is {}".format(
-                    position, machine.occupy.shape
-                )
+            msg = "Machine exceeds boundary, machine position is {} and machine size is {}".format(
+                position, machine.occupy.shape
             )
+            raise BoundaryError(msg)
         self.collide_check()
-        return self.bg
 
     def product_line_img(self, product):
         machine_line = self.product_machines[product]
@@ -98,20 +107,16 @@ class BackgroundTemplate:
             machine = self.machine_dict[machine_code]
             In = self.machine_inandout[machine_code][0]
             img = np.add(img, self.sole_bg[machine_code])
-            plt.plot([In[1],out[1]],[In[0],out[0]],color='red', marker = 'o')
+            plt.plot([In[1], out[1]], [In[0], out[0]], color="red", marker="o")
             out = self.machine_inandout[machine_code][1]
         In = self.out
-        plt.plot([In[1],out[1]],[In[0],out[0]],color='red', marker = 'o')
+        plt.plot([In[1], out[1]], [In[0], out[0]], color="green", marker="o")
         product_line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
         product_line_img[img == 0] = (255, 255, 255)
         product_line_img[img == 1] = (0, 0, 0)
         product_line_img[img >= 2] = (0, 255, 0)
         plt.imshow(product_line_img)
         plt.show()
-            
-        
-            
-            
 
     def bg2img(self):
         img = np.zeros((self.bg.shape[0], self.bg.shape[1], 3), dtype=np.uint8)
@@ -121,6 +126,26 @@ class BackgroundTemplate:
         img[self.bg == 3] = (0, 255, 0)
         img[self.bg == 5] = (0, 0, 255)
         img = plt.imshow(img)
+    
+    def calculate_collision(self):
+        collide_part = np.add(self.bg, -np.ones(self.bg.shape))
+        collide_part[collide_part<0] = 0
+        return np.sum(collide_part)
+    
+    def loss(self, collide_mul = 8):
+        loss = 0
+        for product in self.product_machines:
+            loss += self.product_distance_calculate(product)
+        loss += collide_mul * self.calculate_collision()
+        return loss
+
+
+class BoundaryError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
 
 
 # I think machine class is needed because substances in and out must be defined
@@ -128,16 +153,15 @@ class BackgroundTemplate:
 # In and Out of substance and size of machine and Rules
 if __name__ == "__main__":
     for i in range(2):
-        m1 = np.ones([20+i, 10])
-        m2 = np.ones([10+4*i, 20-i])
-        m3 = np.ones([30+i, 20+i])
-        m4 = np.ones([10, 30+i])
+        m1 = np.ones([20 + i, 10])
+        m2 = np.ones([10 + 4 * i, 20 - i])
+        m3 = np.ones([30 + i, 20 + i])
+        m4 = np.ones([10, 30 + i])
 
-        machine1 = MachineTemplate(m1, 'm1',inandoutpos = [[10,0],[20,10]])
-        print(machine1.In,machine1.out)
-        machine2 = MachineTemplate(m2, 'm2')
-        machine3 = MachineTemplate(m3, 'm3')
-        machine4 = MachineTemplate(m4, 'm4')
+        machine1 = MachineTemplate(m1, "m1", inandoutpos=[[10, 0], [20, 10]])
+        machine2 = MachineTemplate(m2, "m2", inandoutpos=[[10, 0], [0, 15]])
+        machine3 = MachineTemplate(m3, "m3", inandoutpos=[[20, 0], [20, 20]])
+        machine4 = MachineTemplate(m4, "m4", inandoutpos=[[0, 20], [10, 30]])
 
         # machine1.inandout([50,0],[50,100])
         ICN = BackgroundTemplate(np.zeros([100, 100]))
@@ -145,18 +169,18 @@ if __name__ == "__main__":
         ICN.machine_add(machine2)
         ICN.machine_add(machine3)
         ICN.machine_add(machine4)
-        ICN.machine_batch([30, 20], 'm1')
-        ICN.machine_batch([20, 5], 'm2')
-        ICN.machine_batch([50, 40], 'm3')
-        ICN.machine_batch([10, 10], 'm4')
-        ICN.product_line("p1", ['m1', 'm2', 'm3'])
-        ICN.product_line("p2", ['m2', 'm3'])
-        ICN.product_line("p3", ['m3', 'm4', 'm1'])
-        
+        ICN.machine_batch([30, 20], "m1")
+        ICN.machine_batch([20, 5], "m2")
+        ICN.machine_batch([50, 40], "m3")
+        ICN.machine_batch([10, 10], "m4")
+        ICN.product_line("p1", ["m1", "m2", "m3"])
+        ICN.product_line("p2", ["m2", "m3"])
+        ICN.product_line("p3", ["m3", "m4", "m1"])
+
         # print(ICN.machine_dict)
-        print(ICN.product_distance_calculate('p1'))
-        print(ICN.product_distance_calculate('p2'))
-        print(ICN.product_distance_calculate('p3'))
+        print(ICN.product_distance_calculate("p1"))
+        print(ICN.product_distance_calculate("p2"))
+        print(ICN.product_distance_calculate("p3"))
         ICN.product_line_img("p1")
         ICN.product_line_img("p2")
         ICN.product_line_img("p3")
